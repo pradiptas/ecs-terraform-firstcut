f4b549955eeaa9c163e6d30603ea355cdcdeda7f
Pradipta Sanyal(Prad) <pradipta.sanyal@generac.com>
2021-11-01 16:09:06 -0700

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -30,9 +30,3 @@ module "ecs" {
     Environment = "Development"
   }
 }
-
-variable "ecs_cluster_name" {
-  description = "Name of ECS Cluster"
-  type        = string
-  default     = "pradipta-ecs-cluster-terraform-01-dev"
-}
